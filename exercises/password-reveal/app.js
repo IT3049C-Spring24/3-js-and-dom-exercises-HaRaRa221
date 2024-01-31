69d924bc33eb587cb43887028146646d5a39360e
HaRaRa221 <113466996+HaRaRa221@users.noreply.github.com>
2024-01-31 09:28:54 -0500

    cannot solve password reveal

--- exercises/password-reveal/app.js
+++ exercises/password-reveal/app.js
@@ -1,6 +1,6 @@
 // 🤖: create 2 constants with references to the password input and the reveal button from the DOM
 const passwordInput = document.querySelector(`#password`);
-const passwordToggle = document.querySelector(`#password`);
+const passwordToggle = document.querySelector(`#togglepassword`);
 
 // 🤖: Create an Event Listener on the password reveal button
 // 🤖: The event listener should toggle the password field attribute between `password` and `text` to change the visibility of the password field content.
