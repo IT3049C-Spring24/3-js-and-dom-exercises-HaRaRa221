6ab0e5e4e9e576b83d7e5516b3d457fde08afee1
HaRaRa221 <113466996+HaRaRa221@users.noreply.github.com>
2024-01-31 09:07:06 -0500

    could not solve update stat

--- exercises/word-counter/js/app.js
+++ exercises/word-counter/js/app.js
@@ -5,23 +5,13 @@
 // 🤖: The event handler should update the stats section with the number of words and characters in the textarea input.
 // 🤖: You will need to utilize the split method to get the words count.
 // eslint-disable-next-line no-undef
-const textareaInput = document.getElementById(`text`);
+const textArea = document.getElementById(`text`);
 // eslint-disable-next-line no-undef
-const statElem = document.querySelector(`stat`);
+const stats = document.getElementById(`stat`);
 
-textareaInput.addEventListener(`input`, stat_update);
-// eslint-disable-next-line no-undef
-function stat_update() {
-  const charTotal = textareaInput.value;
-  const wordsTotal = charTotal.split(` `);
-  let word_count = 0;
-
-  for (const word of wordsTotal) {
-    if (word.trim() !== ``) {
-      // eslint-disable-next-line no-plusplus
-      word_count++;
-    }
-  }
-  const char = charTotal.length;
-  statElem.textContent = `You've written ${word_count} words and ${char} characters.`;
-}
+textArea.addEventListener(`input`, function () {
+  let textContent = textArea.value;
+  let countWord = textContent.trim().split(` `).length;
+  let countChar = textContent.length;
+  stats.textContent = `You've written ` + countWord + ` words and ` + countChar` characters.`;
+});
